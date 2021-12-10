c7dc5c063ae63ae5c42d743f82bdb3480d6acfe0
Daniel A <53939068+Funkez123@users.noreply.github.com>
2021-12-10 15:07:50 +0100

    Add files via upload

---
+++ rx_arduino_code.ino
@@ -0,0 +1,21 @@
+byte eingang;
+
+#include <SoftwareSerial.h> 
+
+SoftwareSerial mySerial (2,3);
+void setup() {
+
+mySerial.begin(1200);
+Serial.begin(9600);
+}
+
+void loop() {
+
+if (mySerial.available() > 0) {
+
+  eingang = mySerial.read();
+
+Serial.write(eingang);
+
+}
+}
